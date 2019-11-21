7fe85e49ee73bd07e4fba457e45ad3ec393cb4ee
Vladislav Khlebec <vladislavkhlebec@gmail.com>
2019-11-21 15:01:03 +0300

    Updates

---
+++ precache-manifest.941403d0de5a62f4d405112361ef0686.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40664f79e06bef8ee19291cf706b1694",
+    "url": "/students-list/index.html"
+  },
+  {
+    "revision": "024ca9267df32b9f483e",
+    "url": "/students-list/static/css/main.0b7160fd.chunk.css"
+  },
+  {
+    "revision": "ab024cf0b3ddc5c35cbc",
+    "url": "/students-list/static/js/2.cb78d281.chunk.js"
+  },
+  {
+    "revision": "024ca9267df32b9f483e",
+    "url": "/students-list/static/js/main.d4358cfe.chunk.js"
+  },
+  {
+    "revision": "a0de9bf16e9d20514805",
+    "url": "/students-list/static/js/runtime-main.30008668.js"
+  }
+]);
